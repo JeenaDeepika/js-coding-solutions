adc9837ce667e10b276ee92cabd94449df2be046
Deepika Jeena <deepikajeena02@gmail.com>
2025-10-02 12:41:43 +0530

    Create ransom-note.js
    Check if ransomNote can be built from magazine letters

---
+++ ransom-note.js
@@ -0,0 +1,13 @@
+const canConstruct = function(ransomNote, magazine) {
+    const count = {};
+    for (let char of magazine) {
+        count[char] = (count[char] || 0) + 1;
+    }
+    for (let char of ransomNote) {
+        if (!count[char]) {
+            return false;
+        }
+        count[char]--;
+    }
+    return true;
+};
