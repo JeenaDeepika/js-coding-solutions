106addc9bad4a7bc4dcc26b68413df473be771ca
Deepika Jeena <deepikajeena02@gmail.com>
2025-10-02 12:32:27 +0530

    Create missing-number.js
    Find missing number from array using sum formula

---
+++ missing-number.js
@@ -0,0 +1,6 @@
+const missingNumber = function(nums) {
+    const n = nums.length;
+    const expectedSum = (n * (n + 1)) / 2;
+    const actualSum = nums.reduce((acc, num) => acc + num, 0);
+    return expectedSum - actualSum;
+};
