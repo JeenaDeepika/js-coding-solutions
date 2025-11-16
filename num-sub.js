b12820e76f4bf2572b47a7f4ed28aa629a6bc8ad
Deepika Jeena <deepikajeena02@gmail.com>
2025-11-16 21:50:42 +0530

    Create num-sub.js
    Number of Substrings With Only 1s

---
+++ num-sub.js
@@ -0,0 +1,16 @@
+const numSub = function(s) {
+    const MOD = 1e9 + 7;
+    let count = 0; 
+    let result = 0;
+
+    for (let char of s) {
+        if (char === '1') {
+            count++;
+            result = (result + count) % MOD;
+        } else {
+            count = 0;
+        }
+    }
+
+    return result;
+};
