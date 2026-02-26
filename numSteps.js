a60bbc5929fa7af11160878cf2435b0f38143649
Deepika Jeena <deepikajeena02@gmail.com>
2026-02-26 21:37:03 +0530

    1404. Number of Steps to Reduce a Number in Binary Representation to One

---
+++ numSteps.js
@@ -0,0 +1,14 @@
+const numSteps = function(s) {
+    let steps = 0;
+    let carry = 0;
+    for (let i = s.length - 1; i > 0; i--) {
+        const digit = parseInt(s[i]) + carry;
+        if (digit === 1) {
+            steps += 2;
+            carry = 1;
+        } else {
+            steps += 1;
+        }
+    }
+    return steps + carry;
+};
